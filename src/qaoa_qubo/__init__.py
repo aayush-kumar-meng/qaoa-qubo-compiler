856967e1c7e9915262c436ee446de3916188bfbd
aayush-kumar-meng <aayushkumar@uchicago.edu>
2025-12-06 17:05:43 -0600

    Initial project skeleton: src layout + basic files

---
+++ src/qaoa_qubo/__init__.py
@@ -0,0 +1,5 @@
+from .problems import MaxCutProblem, QUBOProblem
+from .qaoa import QAOASolver
+from .result import QAOAResult
+
+__all__ = ["MaxCutProblem", "QUBOProblem", "QAOASolver", "QAOAResult"]
